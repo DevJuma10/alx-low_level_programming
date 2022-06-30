801b6aa55092edfd741f366b3c9a7571b7ace874
Juma <juma.study@gmail.com>
2022-06-30 16:24:39 -0700

    5 description

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,8 @@
-#include <sstdio.h>
+#include <stdio.h>
+/**
+ *main - prints exact characters
+ *Return: Always succes (0)
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
