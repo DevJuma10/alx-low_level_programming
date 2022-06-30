25daa5c849db4f0c1d4f1a174a2af69a2b92a9fc
Juma <juma.study@gmail.com>
2022-06-30 15:57:56 -0700

    SOLUTIONS

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,6 @@
+#include <sstdio.h>
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+};
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+ printf("Size if a char: %d bytes(s)\n", sizeof(char));
+ printf("Size if an int: %d bytes(s)\n", sizeof(int));
+ printf("Size if a long int: %d bytes(s)\n", sizeof(long int));
+ printf("Size if a long long int: %d bytes(s)\n", sizeof(long long int));
+ printf("Size if a float: %d bytes(s)\n", sizeof(float));
+ return (0);
+};
